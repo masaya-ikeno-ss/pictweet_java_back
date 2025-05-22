ce1e6e7eb6474ebb052425c1504c7cf67feae8ba
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-05-22 15:48:23 +0900

    コメント登録APIの引数を修正

--- src/main/java/in/tech_camp/pictweet/controller/CommentController.java
+++ src/main/java/in/tech_camp/pictweet/controller/CommentController.java
@@ -9,9 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,7 +38,7 @@ public class CommentController {
 
   @PostMapping("/")
   public ResponseEntity<?> createComment(@PathVariable("tweetId") Integer tweetId, 
-                            @ModelAttribute("commentForm") @Validated(ValidationOrder.class) CommentForm commentForm,
+                            @RequestBody @Validated(ValidationOrder.class) CommentForm commentForm,
                             BindingResult result,
                             @AuthenticationPrincipal CustomUserDetail currentUser) {
 
